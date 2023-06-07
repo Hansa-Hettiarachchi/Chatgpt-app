b295acc09507dcc5548c64de515f6884535a682a
Hansa-Hettiarachchi <hansahettiarachchi@gmail.com>
2023-06-07 19:52:42 +0530

    config open ai api

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
 import { Configuration, OpenAIApi } from "openai";
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 8080;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+    organization:"org-RCC6nRoqZBKlke7z8RL13EIn",
+    apiKey: "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"
+})
