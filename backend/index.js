2b0821a08e49b0c4c97b61140dae4c5bf01678d3
Hansa-Hettiarachchi <hansahettiarachchi@gmail.com>
2023-06-07 20:05:08 +0530

    post

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,30 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const configuration = new Configuration({
-    organization:"org-RCC6nRoqZBKlke7z8RL13EIn",
-    apiKey: "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"
-})
+  organization: "org-RCC6nRoqZBKlke7z8RL13EIn",
+  apiKey: "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",
+});
 
 const openai = new OpenAIApi(configuration);
 
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+  const results = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are Hansa's GPT-3 chatbot",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    outputs: results.data.choices[0].message,
+  });
+});
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
