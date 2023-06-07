b91c3d9f8184f3f547ef38ea1d8d0f14ccff9a02
Hansa-Hettiarachchi <hansahettiarachchi@gmail.com>
2023-06-07 20:22:29 +0530

    create frontend

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,32 @@
+//import dependencies
 import { Configuration, OpenAIApi } from "openai";
 import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
 
+//create express app
 const app = express();
-const port = 8080;
+//set port
+const port = 8000;
+//use body parser
 app.use(bodyParser.json());
+//use cors
 app.use(cors());
 
+//create configuration
 const configuration = new Configuration({
   organization: "org-RCC6nRoqZBKlke7z8RL13EIn",
-  apiKey: "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",
+  apiKey: "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",
 });
 
+//create openai api
 const openai = new OpenAIApi(configuration);
 
+//create post request to endpoint
 app.post("/", async (request, response) => {
+  //get chats from request body
   const { chats } = request.body;
+  //create chat completion
   const results = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
@@ -28,11 +38,13 @@ app.post("/", async (request, response) => {
     ],
   });
 
+  //send response
   response.json({
     outputs: results.data.choices[0].message,
   });
 });
 
+//listen on port
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
