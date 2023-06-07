5396817923cb268f5496f5d4d557592b273851e9
Hansa-Hettiarachchi <hansahettiarachchi@gmail.com>
2023-06-07 19:54:50 +0530

    cofig port

--- backend/index.js
+++ backend/index.js
@@ -12,3 +12,9 @@ const configuration = new Configuration({
     organization:"org-RCC6nRoqZBKlke7z8RL13EIn",
     apiKey: "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"
 })
+
+const openai = new OpenAIApi(configuration);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
